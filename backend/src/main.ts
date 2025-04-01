dc2e30f2932926b26a49cc289c9a298ae63544f8
Ilya Konoplitsky <96144903+IlyaKonoplitsky@users.noreply.github.com>
2025-04-01 13:12:50 +0500

    feat: initial web

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import * as YAML from 'yaml';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 10002;
@@ -10,8 +12,25 @@ async function bootstrap() {
   const config = new DocumentBuilder().setTitle('Attraction Map').setVersion('1.0').build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api', app, document);
+
+  // Генерируем YAML-файл
+  const yamlDocument = YAML.stringify(document);
+  writeFileSync('./swagger.yaml', yamlDocument);
+
+  // Настраиваем Swagger UI
+  SwaggerModule.setup('/api', app, document, {
+    swaggerOptions: {
+      urls: [{ url: '/api-docs.yaml', name: 'Swagger YAML' }]
+    }
+  });
+
+  // Раздаём YAML через Express
+  app.use('/api-docs.yaml', (_, res) => {
+    res.setHeader('Content-Type', 'text/yaml');
+    res.send(yamlDocument);
+  });
 
   await app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }
+
 bootstrap();
